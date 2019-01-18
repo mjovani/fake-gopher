730bd1d470011a4b585521fd9d3abce45f57d571
kleoxhindoli <kxhindoli@helius-software.com>
2019-01-18 13:26:36 +0100

    feat: workspaces

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const fs = require('fs')
 
 
 const videos = require('./videos.json')
@@ -28,31 +29,55 @@ app.post('/services/gopher/query/workspace/:clWorkspaceId/revisions', jsonParser
     const query = req.body.queryRequest
     console.log(JSON.stringify(query))
     if (JSON.stringify(query).includes('external-video')) {
+        const response = getFromWorkspace('external-video', req.params.clWorkspaceId)
+        if (response) {
+            return res.send(JSON.parse(response))
+        }
         let v = JSON.parse(JSON.stringify(externalVideos))
         // a.assets = a.assets.map(asset => asset.asset)
         return res.send(v)
     }
     else if (JSON.stringify(query).includes('video')) {
+        const response = getFromWorkspace('video', req.params.clWorkspaceId)
+        if (response) {
+            return res.send(JSON.parse(response))
+        }
         let v = JSON.parse(JSON.stringify(externalVideos))
         // v.assets = v.assets.map(asset => asset.asset)
         return res.send(v)
     }
     else if (JSON.stringify(query).includes('audio')) {
+        const response = getFromWorkspace('audio', req.params.clWorkspaceId)
+        if (response) {
+            return res.send(JSON.parse(response))
+        }
         let a = JSON.parse(JSON.stringify(audios))
         // a.assets = a.assets.map(asset => asset.asset)
         return res.send(a)
     }
     else if (JSON.stringify(query).includes('broadsheet')) {
+        const response = getFromWorkspace('broadsheet', req.params.clWorkspaceId)
+        if (response) {
+            return res.send(JSON.parse(response))
+        }
         console.log(JSON.stringify(broadsheets))
         // a.assets = a.assets.map(asset => asset.asset)
         return res.send(broadsheets)
     }
     else if (JSON.stringify(query).includes('cars')) {
+        const response = getFromWorkspace('cars', req.params.clWorkspaceId)
+        if (response) {
+            return res.send(JSON.parse(response))
+        }
         console.log(JSON.stringify(cars))
         // a.assets = a.assets.map(asset => asset.asset)
         res.send(cars);
     }
     else {
+        const response = getFromWorkspace('images', req.params.clWorkspaceId)
+        if (response) {
+            return res.send(JSON.parse(response))
+        }
         let i = JSON.parse(JSON.stringify(images))
         console.log(JSON.stringify(i))
         // i.assets = i.assets.map(asset => asset.asset)
@@ -60,6 +85,15 @@ app.post('/services/gopher/query/workspace/:clWorkspaceId/revisions', jsonParser
     }
 })
 
+
+function getFromWorkspace(type, workspace) {
+    const file = `/worksapces/${workspace}/${type}.json`
+    if(fs.existsSync(file)) {
+        const contents = fs.readFileSync(file)
+        return JSON.parse(contents)
+    }
+    return null
+}
 
 
 app.listen(8787, () => console.log('Example app listening on port 8787!'))
